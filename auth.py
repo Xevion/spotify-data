19a4afb90904e178ce8c0c150079cb50e1d7916a
Xevion <xevioni@yandex.com>
2019-10-27 19:29:32 -0500

    fix accidental auth leak

--- auth.py
+++ auth.py
@@ -1,7 +1,5 @@
-username = "xevioni"
-client_id = "20cc828edc7140d88e1c8b0c08ed1672"
-client_secret = "519fb97148a14952859ca3454b886cf9"
-redirect_uri = "http://localhost:8888/callback"
-scope = ' '.join([
-    'user-library-read'
-])
+username = ""
+client_id = ""
+client_secret = ""
+redirect_uri = ""
+scope = ' '.join([])
